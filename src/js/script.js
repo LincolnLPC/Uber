ee735688c17c8fd38e8eb4f3a0ac4d89bd8ccf5c
Lincoln <Linc.LPC.1@gmail.com>
2021-03-27 15:00:02 +0500

    First versoin

---
+++ src/js/script.js
@@ -0,0 +1,101 @@
+﻿/*
+var FirstName=prompt("Как вас зовут?");
+var Family=prompt("Введите фамилию");
+var Age=prompt("Ваш возраст");
+console.log("Пользователь:" + FirstName +" " + Family);
+console.log("Возраст:"+Age);
+var dollar=74.44;
+var rub;
+var rub=prompt("Сколько Долларов вы хотите поменят?");
+alert("Вы получите: "+dollar*rub+" Рублей");*/
+
+/*var heightBody=prompt("какой у вас рост?")
+var ages=prompt("Ваш возраст?")
+var heightcube=heightBody*heightBody
+var bmi=massBody/heightcube
+/*var answer= bmi>28.90
+alert(answer)
+
+if(ages>=18&&ages<=25&&bmi<=17.5){
+	alert("Вес недостаточен опасно для здоровья");
+}else if(ages>=18&&ages<=25&&bmi>=17.5&&bmi<=19.5){
+	alert("Вес слегка снижен не опасно для здоровья");
+}else if(ages>=18&&ages<=25&&bmi>=19.5){
+	alert("Вес Нормальный");
+}else if(ages>=18&&ages<=25&&bmi>=23.0){
+	alert("Есть излишний вес");
+}else if(ages>=18&&ages<=25&&bmi>=27.5){
+	alert("Ожирение 1 степени");
+}else if(ages>=18&&ages<=25&&bmi>=30.0){
+	alert("Ожирение 2 степени");	
+}else if(ages>=18&&ages<=25&&bmi>=35.0){
+	alert("Ожирение 3 степени");	
+}else if(ages>=18&&ages<=25&&bmi>=40.0){
+	alert("Ожирение 4 степени");	
+}else if(ages>=26&&ages<=46&&bmi>18.0){
+
+}else if(ages>=26&&ages<=46&&bmi<=18.0){
+	
+}else if(ages>=26&&ages<=46&&bmi<=18.0){
+	
+}else if(ages>=26&&ages<=46&&bmi<=18.0){
+	
+}else if(ages>=26&&ages<=46&&bmi<=18.0){
+	
+}else if(ages>=26&&ages<=46&&bmi<=18.0){
+	
+}else if(ages>=26&&ages<=46&&bmi<=18.0){
+	
+}else if(ages>=26&&ages<=46&&bmi<=18.0){
+	
+}else if(ages>=26&&ages<=46&&bmi<=18.0){
+	
+}else if(ages>=26&&ages<=46&&bmi<=18.0){
+	
+};
+*/
+
+
+
+/*
+var login=prompt("Введите ваш логин")
+var	pass=prompt("Введите ваш пароль")
+
+
+switch(true) {
+	 case login==="YouRa"&&pass==="12345":
+	 alert("Добро пожаловать Юрий");
+     break;
+	 case login==="Anastasia"&&pass==="11111":
+	 alert("Добро пожаловать Настенька");
+	 break;
+	 case login==="Admin"&& pass==="22222":
+	 alert("Вы вошли как админ");
+	 break;
+	 default:
+	 alert("Вы ввели не правильно логин или пароль")
+}
+
+*/
+
+
+
+
+var y=1
+
+
+while (y<10){
+	console.log(y)
+	y++
+}
+
+
+var x=1
+
+while (x<10){
+
+console.log(x)
+
+x += 2
+
+}
